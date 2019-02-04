a2d87af9d31ce6d8e263b89cc5d5ee7207f4513e
Victor Golubei <viktorgolubey@hotmail.com>
2019-02-04 22:19:58 +0100

    improved the the replacement script

--- src/executer.js
+++ src/executer.js
@@ -8,22 +8,10 @@ chrome.storage.sync.get(
         if(data.replacementData != null && data.replacementData.urlToBeApplied != null) {
             var urlregex = new RegExp(escapeRegexSpecialChars(data.replacementData.urlToBeApplied), "g");
             if(locationUrl.match(urlregex) != null) {
-                var replacementDetails;
-                
+                    var replacementDetails;
                     replacementDetails = data.replacementData.replacements;
-
-                    Object.keys(replacementDetails).forEach((key) => {
-                        console.log("search " +  replacementDetails[key].search  + ", replace with " + replacementDetails[key].replace)
-                        replaceValuesInHtmlDocumentTextContent(document.body, replacementDetails[key].search, replacementDetails[key].replace);
-                        //document.body.innerHTML = document.body.innerHTML.replace(new RegExp(replacementDetails[key].search, "gi"), replacementDetails[key].replace);
-                        /*var matches = document.querySelectorAll("*:not(html):not(head):not(script):not(meta):not(link)");
-                        console.log(matches);
-                        [].forEach.call(matches, function(elem) {
-                        var text = ('innerText' in elem) ? 'innerText' : 'textContent';
-                        //elem[text] = elem[text].replace(new RegExp(key, "gi"), replacementDetails[key]);
-                        elem[text] = elem[text].replace("miloane", replacementDetails[key]);
-                        });*/
-                    });
+                    replaceValuesInHtmlDocumentTextContent(document.body, replacementDetails);
+                    
             }
         }
     }
@@ -34,16 +22,19 @@ function escapeRegexSpecialChars(textToEscape)
     return textToEscape.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
-function replaceValuesInHtmlDocumentTextContent(node, search, replace) {
+function replaceValuesInHtmlDocumentTextContent(node, replacementDetails) {
  
     var nodeList = node.childNodes;
 
     for (var x = 0; x < nodeList.length; x++) {
         // text node, search directly
-        if (nodeList[x].nodeType == 3) 
-            nodeList[x].textContent = nodeList[x].textContent.replace(new RegExp(search, "gi"), replace);
+        if (nodeList[x].nodeType == 3) {
+            Object.keys(replacementDetails).forEach((key) => {
+                nodeList[x].textContent = nodeList[x].textContent.replace(new RegExp(replacementDetails[key].search, "gi"), replacementDetails[key].replace);
+            });
+        }
         else 
-            replaceValuesInHtmlDocumentTextContent(nodeList[x], search, replace);
+            replaceValuesInHtmlDocumentTextContent(nodeList[x], replacementDetails);
     }
 
 }
